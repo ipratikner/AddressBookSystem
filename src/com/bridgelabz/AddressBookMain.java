0b96441daad44fdf1c133b28354e3296342b1bdb
ipratikner <pratiknerlikar@gmail.com>
2021-11-25 19:41:55 +0530

    Added: deleted existing contact by name

--- src/com/bridgelabz/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookMain.java
@@ -21,7 +21,7 @@ public class AddressBookMain {
     public void addContacts() {
 
         System.out.println("Enter your choice");
-        System.out.println("1 : Add new contact    2 : Edit contact ");
+        System.out.println("1 :Add new contact    2 :Edit contact  3 :Delete contact");
         int choice = sc.nextInt();
         switch (choice) {
             case 1:
@@ -86,6 +86,15 @@ public class AddressBookMain {
                 addressbooks.editContact(nameToEdit, editFirstName, editLastName, editAddress, editCity, editState, editZip,
                         editPhoneNumber, editEmail);
                 addressbooks.addContacts();
+
+            case 3:
+                System.out.println("Enter the person Name");
+                String deletename = sc.next();
+                addressbooks.deleteContactDetails(deletename);
+                addressbooks.addContacts();
+                break;
+            default:
+                System.out.println("Please Enter correct choice");
         }
 
     }
@@ -130,6 +139,20 @@ public class AddressBookMain {
                 System.out.println("No Conatct Details available :");
             }
         }
+
+    }
+
+    public void deleteContactDetails(String name) {
+        List<Contacts> contactDetails = addressbook.getContacts();
+        for (int i = 0; i <= contactDetails.size() - 1; i++) {
+            Contacts contactperson = contactDetails.get(i);
+            if (contactperson.getFirstName().equals(name)) {
+                contactDetails.remove(i);
+                addressbook.setContacts(contactDetails);
+            }
+        }
+        System.out.println("Contact deleted Successfully");
+        addressbooks.displayContacts(addressbook);
     }
 }
 
