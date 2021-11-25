d61e888f19c3e518ef6d2316219af3b9d5617516
ipratikner <pratiknerlikar@gmail.com>
2021-11-25 19:30:03 +0530

    Added: new  contact in addressbook with first and last name, city, zip, state, mobile number and email

--- src/com/bridgelabz/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookMain.java
@@ -1,7 +1,48 @@
 package com.bridgelabz;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class AddressBookMain {
+    private static Scanner sc = new Scanner(System.in);
+    private AddressBook addressbook = new AddressBook();
+    private List<Contacts> contacts = new ArrayList<>();
+
     public static void main(String[] args) {
         System.out.println("Welcome to AddressBook Program");
+
+        AddressBookMain addressbook = new AddressBookMain();
+        addressbook.addContacts();
+
+    }
+
+    public void addContacts() {
+
+        System.out.println("Enter the First Name");
+        String firstName = sc.next();
+
+        System.out.println("Enter the Last Name");
+        String lastName = sc.next();
+
+        System.out.println("Enter the Address Name");
+        String address = sc.next();
+
+        System.out.println("Enter the City Name");
+        String city = sc.next();
+
+        System.out.println("Enter the State Name");
+        String state = sc.next();
+
+        System.out.println("Enter the Zip Name");
+        int zip = sc.nextInt();
+
+        System.out.println("Enter the PhoneNumber");
+        long phoneNumber = sc.nextInt();
+
+        System.out.println("Enter the email");
+        String email = sc.next();
+        Contacts contact = new Contacts(firstName, lastName, address, city, state, zip, phoneNumber, email);
+        contacts.add(contact);
+        addressbook.setContacts(contacts);
     }
 }
