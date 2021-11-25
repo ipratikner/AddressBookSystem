320acb156a9e0658e5a48ab9e44e41b65f7d9be1
ipratikner <pratiknerlikar@gmail.com>
2021-11-25 19:52:03 +0530

    Added: refactored to add multiple contacts in addressbook to the system

--- src/com/bridgelabz/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookMain.java
@@ -1,13 +1,17 @@
 package com.bridgelabz;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Scanner;
 
 public class AddressBookMain {
     private static Scanner sc = new Scanner(System.in);
-    private static AddressBook addressbook = new AddressBook();
-    private List<Contacts> contacts = new ArrayList<>();
+    private static AddressBook addressbookValue = new AddressBook();
+
     private static AddressBookMain addressbooks = new AddressBookMain();
+    private Map<String, AddressBook> addressBookSystem = new HashMap<>();
 
 
     public static void main(String[] args) {
@@ -21,10 +25,15 @@ public class AddressBookMain {
     public void addContacts() {
 
         System.out.println("Enter your choice");
-        System.out.println("1 : Add new contact    2 : Edit contact  3 : Delete contact  4: Add Multiple Contacts");
+        System.out.println(
+                "1 : Add new contact    2 : Edit contact  3 : Delete contact  4: Add Multiple Contacts 5: Display Contacts");
         int choice = sc.nextInt();
         switch (choice) {
             case 1:
+
+                List<Contacts> contacts = new ArrayList<>();
+                System.out.println("Enter Your Address Book name ");
+                String addresBookname = sc.next();
 
                 System.out.println("Enter the First Name");
                 String firstName = sc.next();
@@ -49,14 +58,18 @@ public class AddressBookMain {
 
                 System.out.println("Enter the email");
                 String email = sc.next();
+
                 Contacts contact = new Contacts(firstName, lastName, address, city, state, zip, phoneNumber, email);
                 contacts.add(contact);
-                addressbook.setContacts(contacts);
-                addressbooks.displayContacts(addressbook);
+                addressbookValue.setContacts(contacts);
+                addressBookSystem.put(addresBookname, addressbookValue);
                 addressbooks.addContacts();
 
                 break;
             case 2:
+                System.out.println("Enter Your Address Book name ");
+                String findAddressBook = sc.next();
+
                 System.out.println("Enter the name of user to Edit");
                 String nameToEdit = sc.next();
 
@@ -83,19 +96,26 @@ public class AddressBookMain {
 
                 System.out.println("Enter the email");
                 String editEmail = sc.next();
-                addressbooks.editContact(nameToEdit, editFirstName, editLastName, editAddress, editCity, editState, editZip,
-                        editPhoneNumber, editEmail);
+                addressbooks.editContact(findAddressBook, nameToEdit, editFirstName, editLastName, editAddress, editCity,
+                        editState, editZip, editPhoneNumber, editEmail);
                 addressbooks.addContacts();
 
             case 3:
+                System.out.println("Enter Your Address Book name ");
+                String yourBookname = sc.next();
                 System.out.println("Enter the person Name");
                 String deletename = sc.next();
-                addressbooks.deleteContactDetails(deletename);
+
+                addressbooks.deleteContactDetails(deletename, yourBookname);
                 addressbooks.addContacts();
                 break;
             case 4:
 
                 addressbooks.addMultipleContacts();
+                addressbooks.addContacts();
+                break;
+            case 5:
+                addressbooks.displayContacts(addressBookSystem);
                 addressbooks.addContacts();
                 break;
             default:
@@ -104,9 +124,14 @@ public class AddressBookMain {
 
     }
 
-    public void editContact(String name, String editFirstName, String editLastName, String editAddress, String editCity,
-                            String editState, int editZip, long editPhoneNumber, String editEmail) {
-        List<Contacts> contactDetails = addressbook.getContacts();
+    public void editContact(String findAddressBook, String name, String editFirstName, String editLastName,
+                            String editAddress, String editCity, String editState, int editZip, long editPhoneNumber,
+                            String editEmail) {
+
+        AddressBook addressbook = new AddressBook();
+
+        AddressBook addressbookdetails = addressBookSystem.get(findAddressBook);
+        List<Contacts> contactDetails = addressbookdetails.getContacts();
         for (int i = 0; i <= contactDetails.size() - 1; i++) {
 
             Contacts contactperson = contactDetails.get(i);
@@ -122,45 +147,63 @@ public class AddressBookMain {
                 contactperson.setEmail(editEmail);
                 contactDetails.set(i, contactperson);
                 addressbook.setContacts(contactDetails);
-
+                addressBookSystem.put(findAddressBook, addressbook);
             }
         }
-        addressbooks.displayContacts(addressbook);
+        addressbooks.displayContacts(addressBookSystem);
     }
 
-    public void displayContacts(AddressBook addressBook) {
-        List<Contacts> contactDetails = addressBook.getContacts();
+    public void displayContacts(Map<String, AddressBook> addressBookSystem) {
 
-        for (int i = 0; i <= contactDetails.size() - 1; i++) {
-            Contacts contactperson = contactDetails.get(i);
-            if (!contactperson.getFirstName().equals("")) {
-                System.out.println("Conatct Details :");
-                System.out.println("FirstName :" + contactperson.getFirstName() + "      LastName :"
-                        + contactperson.getLastName() + "     Address :" + contactperson.getAddress() + "      City :"
-                        + contactperson.getCity() + "       State :" + contactperson.getState() + "     Zip :"
-                        + contactperson.getZip() + "     Phone Number :" + contactperson.getPhoneNumber());
-            } else {
+        for (Entry<String, AddressBook> set : addressBookSystem.entrySet()) {
 
-                System.out.println("No Conatct Details available :");
+            AddressBook values = set.getValue();
+            List<Contacts> contactDetails = values.getContacts();
+
+            for (int i = 0; i <= contactDetails.size() - 1; i++) {
+                Contacts contactperson = contactDetails.get(i);
+                if (!contactperson.getFirstName().equals("")) {
+
+                    System.out.println("Conatct Details :");
+                    System.out.println("Book  Name --- " + set.getKey());
+                    System.out.println("FirstName :" + contactperson.getFirstName() + "      LastName :"
+                            + contactperson.getLastName() + "     Address :" + contactperson.getAddress()
+                            + "      City :" + contactperson.getCity() + "       State :" + contactperson.getState()
+                            + "     Zip :" + contactperson.getZip() + "     Phone Number :"
+                            + contactperson.getPhoneNumber());
+                } else {
+
+                    System.out.println("No Conatct Details available :");
+                }
             }
         }
 
     }
 
-    public void deleteContactDetails(String name) {
-        List<Contacts> contactDetails = addressbook.getContacts();
+    public void deleteContactDetails(String name, String yourBookname) {
+
+        AddressBook addressbook = new AddressBook();
+
+        AddressBook values = addressBookSystem.get(yourBookname);
+        List<Contacts> contactDetails = values.getContacts();
+
         for (int i = 0; i <= contactDetails.size() - 1; i++) {
             Contacts contactperson = contactDetails.get(i);
             if (contactperson.getFirstName().equals(name)) {
                 contactDetails.remove(i);
                 addressbook.setContacts(contactDetails);
+                addressBookSystem.put(yourBookname, addressbook);
             }
         }
+
         System.out.println("Contact deleted Successfully");
-        addressbooks.displayContacts(addressbook);
     }
 
     public void addMultipleContacts() {
+
+        List<Contacts> contacts = new ArrayList<>();
+        System.out.println("Enter Your Address Book name ");
+        String findAddressBook = sc.next();
 
         System.out.println("Enter the First Name");
         String firstName = sc.next();
@@ -187,7 +230,8 @@ public class AddressBookMain {
         String email = sc.next();
         Contacts contact = new Contacts(firstName, lastName, address, city, state, zip, phoneNumber, email);
         contacts.add(contact);
-        addressbook.setContacts(contacts);
+
+        addtoOurAdrressBook(contact, findAddressBook);
 
         System.out.println("Do you want add one more Contact Details");
         System.out.println("1 :Yes     2 :No");
@@ -198,7 +242,7 @@ public class AddressBookMain {
                 addressbooks.addMultipleContacts();
                 break;
             case 2:
-                addressbooks.displayContacts(addressbook);
+                addressbooks.displayContacts(addressBookSystem);
                 addressbooks.addContacts();
                 break;
             default:
@@ -206,9 +250,27 @@ public class AddressBookMain {
 
         }
 
-        addressbooks.displayContacts(addressbook);
+        addressbooks.displayContacts(addressBookSystem);
         addressbooks.addContacts();
 
+    }
+
+    public void addtoOurAdrressBook(Contacts contact, String addressBookName) {
+        List<Contacts> contactsLis = new ArrayList<>();
+        AddressBook Addressvalues = new AddressBook();
+        boolean isKeyPresent = addressBookSystem.containsKey(addressBookName);
+        if (isKeyPresent) {
+            AddressBook values = addressBookSystem.get(addressBookName);
+            addressBookSystem.get(addressBookName);
+            List<Contacts> contactDetails = values.getContacts();
+            contactDetails.add(contact);
+            values.setContacts(contactDetails);
+            addressBookSystem.put(addressBookName, values);
+        } else {
+            contactsLis.add(contact);
+            Addressvalues.setContacts(contactsLis);
+            addressBookSystem.put(addressBookName, Addressvalues);
+        }
     }
 }
 
