2a54c55d7d5fbf18c4dd034e76bbc8d7a32b27b3
ipratikner <pratiknerlikar@gmail.com>
2021-11-25 19:19:44 +0530

    Added: created contact in addressbook with first and last name, city, zip, state, mobile number and email

---
+++ src/com/bridgelabz/AddressBook.java
@@ -0,0 +1,14 @@
+package com.bridgelabz;
+import java.util.List;
+
+public class AddressBook {
+    private List<Contacts> contacts;
+
+    public List<Contacts> getContacts() {
+        return contacts;
+    }
+
+    public void setContacts(List<Contacts> contacts) {
+        this.contacts = contacts;
+    }
+}
